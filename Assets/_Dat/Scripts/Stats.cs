5d9f71fdddbcfa3fafefa4cd08fd1806e146f49f
kdat2109 <khongminhdat480@gmail.com>
2025-05-28 14:50:22 +0700

    them code di chuyen cho nhan vat

---
+++ Assets/_Dat/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [Serializable]
+    public class Data
+    {
+        public float health;
+        public float damage;
+        public float speed;
+        public float attackSpeed;
+    }
+    public Data data;
+}
