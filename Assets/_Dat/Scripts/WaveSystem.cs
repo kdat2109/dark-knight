b5ea165073ab1beb776a9fd0838bad771755c7dc
dat2109 <khongminhdat480@gmail.com>
2025-06-11 10:14:20 +0700

    sua wave

--- Assets/_Dat/Scripts/WaveSystem.cs
+++ Assets/_Dat/Scripts/WaveSystem.cs
@@ -8,8 +8,7 @@ namespace _Dat
 {
     public class WaveSystem : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject[] enemies;
+        
         [SerializeField]
         private Transform[] spawnPoint;
         [SerializeField]
@@ -17,28 +16,89 @@ namespace _Dat
 
         [SerializeField]
         private float timeEndWave;
+        
+        public DataWave[] waves;
+        
+        int currentWave = 0;
 
         private void Start()
         {
             StartCoroutine(EndWave());
-            StartCoroutine(SpawnEnemies());
+            StartCoroutine(SpawnEnemies(waves[currentWave]));
         }
 
+
+
+        /*IEnumerator SpawnEnemies(DataWave wave)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(timeSpawn);    
+                
+                int indexEnemy = Random.Range(0, wave.enemies.Length);
+                GameObject enemy = wave.enemies[indexEnemy];
+                
+                int randomCount = Random.Range(wave.min, wave.max);
+                
+                for (int i = 0; i < randomCount; i++)
+                {
+                    int indexPoint = Random.Range(0, spawnPoint.Length);
+
+                    float randomX = Random.Range(-2, 2);
+                    float randomY = Random.Range(-2, 2);
+                    
+                    Vector2 spawnPos = (Vector2)spawnPoint[indexPoint].position + new Vector2(randomX, randomY);
+                    
+                    Instantiate(enemy, spawnPos, Quaternion.identity);
+                }
+            }
+        }*/
+        
+        IEnumerator SpawnEnemies(DataWave wave)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(timeSpawn);    
+
+                int randomCount = Random.Range(wave.min, wave.max); // Số lượng quái sẽ sinh
+
+                // Chọn 1 spawn point duy nhất cho lần này
+                int indexPoint = Random.Range(0, spawnPoint.Length);
+                Vector2 baseSpawnPos = spawnPoint[indexPoint].position;
+
+                for (int i = 0; i < randomCount; i++)
+                {
+                    // Chọn loại quái ngẫu nhiên cho mỗi lần sinh
+                    int indexEnemy = Random.Range(0, wave.enemies.Length);
+                    GameObject enemy = wave.enemies[indexEnemy];
+
+                    // Tạo thêm vị trí ngẫu nhiên nhỏ xung quanh spawnPoint
+                    float randomX = Random.Range(-2f, 2f);
+                    float randomY = Random.Range(-2f, 2f);
+                    Vector2 spawnPos = baseSpawnPos + new Vector2(randomX, randomY);
+
+                    Instantiate(enemy, spawnPos, Quaternion.identity);
+                }
+            }
+        }
+
+        
+        
+        
+        
+        
         IEnumerator EndWave()
         {
             yield return new WaitForSeconds(timeEndWave);
             StopAllCoroutines();
         }
 
-        IEnumerator SpawnEnemies()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(timeSpawn);    
-                int indexEnemy = Random.Range(0, enemies.Length);
-                int indexPoint = Random.Range(0, spawnPoint.Length);
-                Instantiate(enemies[indexEnemy], spawnPoint[indexPoint].position, Quaternion.identity);
-            }
-        }
+    }
+    [Serializable]
+    public class DataWave
+    {
+        public GameObject[] enemies;
+        public int min;
+        public int max;
     }
 }
